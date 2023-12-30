8ba80a4ab9b6c4c2e182be27e7a7e2bbaf99ac1b
Trent McCarthy <74326941+tmac006@users.noreply.github.com>
2023-12-29 19:25:01 -0500

    Did A lot of stuff
    Added interface for when you are typing in the date, made the time scale not properly but good enough. MORE TO COME hopefully...

--- Main.java
+++ Main.java
@@ -1,80 +1,111 @@
 import javax.swing.*;
 import java.awt.*;
 import java.text.ParseException;
-import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicReference;
 
-public class Main
-{
-
-    public static void main(String[] args)
-    {
-        try
-        {
-            // Enter the future date
-            System.out.println("Enter the future date (yyyy-MM-dd HH:mm:ss): ");
-            String inputDate = new Scanner(System.in).nextLine();
-
-            JFrame frame = new JFrame("Countdown Timer");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-            JPanel panel = new JPanel();
-            frame.getContentPane().add(panel);
-            placeComponents(panel, inputDate);
-
-            frame.setSize(400, 200);
-            frame.setVisible(true);
-
-            while (true)
-            {
-                // Difference Calculation
-                long timeDifference = DateCalculator.calculateTimeDifference(inputDate);
-
-                if (timeDifference <= 0)
-                {
-                    System.out.println("The specified time has passed.");
-                    break;
-                }
-
-                // Conversion Code
-                long[] timeComponents = DateCalculator.convertMilliseconds(timeDifference);
-
-                // Update the GUI
-                updateGUI(panel, timeComponents);
-
-                // Updates the countdown
-                Thread.sleep(1000);
-            }
-        }
-        catch (ParseException | InterruptedException e)
-        {
-            System.out.println("Error: " + e.getMessage());
-        }
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(Main::createAndShowGUI);
     }
 
-    private static void placeComponents(JPanel panel, String inputDate)
-    {
-        panel.setLayout(new BorderLayout());
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("Countdown Timer");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        frame.getContentPane().add(panel);
+        placeComponents(panel);
+
+        frame.setSize(400, 200);
+        frame.setVisible(true);
+    }
+
+    private static void placeComponents(JPanel panel) {
+        panel.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
 
         JLabel countdownLabel = new JLabel("Countdown Timer", SwingConstants.CENTER);
         countdownLabel.setFont(new Font("Arial", Font.BOLD, 24));
-        panel.add(countdownLabel, BorderLayout.NORTH);
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 2;
+        panel.add(countdownLabel, gbc);
 
-        JLabel dateLabel = new JLabel("Time Until " + inputDate, SwingConstants.CENTER);
-        countdownLabel.setFont(new Font("Arial", Font.BOLD, 24));
-        panel.add(dateLabel, BorderLayout.CENTER);
+        JPanel inputPanel = new JPanel();
+        JTextField dateTextField = new JTextField("yyyy-MM-dd HH:mm:ss");
+        JButton startButton = new JButton("Start Countdown");
+        //This is a comment
+        AtomicReference<String> inputDateRef = new AtomicReference<>();
 
-        JLabel timeLabel = new JLabel("", SwingConstants.CENTER);
-        timeLabel.setFont(new Font("Arial", Font.BOLD, 60));
-        panel.add(timeLabel, BorderLayout.SOUTH);
+        startButton.addActionListener(e -> {
+            String inputDate = dateTextField.getText();
+            inputDateRef.set(inputDate);
+            startCountdown(panel, inputDateRef);
+            inputPanel.setVisible(false);
+        });
+
+        inputPanel.add(dateTextField);
+        inputPanel.add(startButton);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 2;
+        panel.add(inputPanel, gbc);
+
+        JLabel timeUntilLabel = new JLabel("", SwingConstants.CENTER);
+        timeUntilLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.gridwidth = 2;
+        panel.add(timeUntilLabel, gbc);
     }
 
-    private static void updateGUI(JPanel panel, long[] timeComponents)
-    {
-        JLabel timeLabel = (JLabel) panel.getComponent(2); // Assumes the time label is the third component
+    private static void startCountdown(JPanel panel, AtomicReference<String> inputDateRef) {
+        new Thread(() -> {
+            try {
+                while (true) {
+                    String inputDate = inputDateRef.get();
+                    long timeDifference = DateCalculator.calculateTimeDifference(inputDate);
 
-        String formattedTime = String.format("%d days, %d hours, %d minutes, %d seconds",
-                timeComponents[0], timeComponents[1], timeComponents[2], timeComponents[3]);
+                    if (timeDifference <= 0) {
+                        System.out.println("The specified time has passed.");
+                        break;
+                    }
 
-        timeLabel.setText(formattedTime);
+                    long[] timeComponents = DateCalculator.convertMilliseconds(timeDifference);
+
+                    updateGUI(panel, inputDate, timeComponents);
+
+                    Thread.sleep(1000);
+                }
+            } catch (ParseException | InterruptedException ex) {
+                System.out.println("Error: " + ex.getMessage());
+            }
+        }).start();
+    }
+
+    private static void updateGUI(JPanel panel, String inputDate, long[] timeComponents) {
+        JLabel timeLabel = findTimeLabel(panel);
+
+        if (timeLabel != null) {
+            String formattedTime = String.format("Time Until %s - %d days, %d hours, %d minutes, %d seconds",
+                    inputDate, timeComponents[0], timeComponents[1], timeComponents[2], timeComponents[3]);
+
+            SwingUtilities.invokeLater(() -> {
+                timeLabel.setText(formattedTime);
+                panel.revalidate();
+            });
+        }
+    }
+
+    private static JLabel findTimeLabel(JPanel panel) {
+        Component[] components = panel.getComponents();
+        for (Component component : components) {
+            if (component instanceof JLabel) {
+                return (JLabel) component;
+            }
+        }
+        return null;
     }
 }
